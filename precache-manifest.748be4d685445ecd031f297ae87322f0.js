2addb1a1213f36a5c3c110f7b62bf43f07dc846d
Magnus Anderssen <magnus@magooweb.com>
2018-11-08 21:03:15 +0100

    Updates

---
+++ precache-manifest.748be4d685445ecd031f297ae87322f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "d1d8a9eeef1b84e68d62",
+    "url": "/tic-tac-toe/static/js/main.d1d8a9ee.chunk.js"
+  },
+  {
+    "revision": "54c87ab5c77b74d79366",
+    "url": "/tic-tac-toe/static/js/1.54c87ab5.chunk.js"
+  },
+  {
+    "revision": "d1d8a9eeef1b84e68d62",
+    "url": "/tic-tac-toe/static/css/main.58ac4f72.chunk.css"
+  },
+  {
+    "revision": "726ac5d351bc1bbf3fe5da5e4a6f7e1f",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
